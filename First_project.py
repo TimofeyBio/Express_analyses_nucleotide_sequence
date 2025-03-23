f4b65abc0ad894cee6046f8b0f958c8b4c235e40
Тимофей <karavaev.t.a@gmail.com>
2025-03-23 13:54:53 +0300

    Add work with file .fasta

--- First_project.py
+++ First_project.py
@@ -1,33 +1,48 @@
 from collections import Counter     # Модуль для подсчёта элементов
 
 def nucleotide_counting(Seq):   # Функция подсчёФта нуклеотидов и длины последовательности
-    return Counter(Seq), len(Seq)
+    return Counter(Seq), int(len(Seq))
 
-Seq_DNA = str(input()).upper()  # Ввод последовательности и её исправление на заглавные буквы
+file_name = input("Enter the path of the file: ")     # Ввод полного пути до читаемого файла
 
-if not Seq_DNA:     # Проверка на пустую строку
-    print("ERROR - empty value")
-else:
-    invalid_nucleotides = []
+with open(file_name, "r") as Seq_DNA:    # Открытие файла
+    New_seq_DNA = str('')
+    s = [str(i) for i in Seq_DNA.readlines()]
 
-    # Поиск некорректных символов и их позиции
-    for position, nuc in enumerate(Seq_DNA, start=1):
-        if nuc not in 'GCAT':
-            invalid_nucleotides.append((nuc, position))
-
-    # Вывод найденых некорректных символов и их позиций
-    if invalid_nucleotides:
-        for nuc, position in invalid_nucleotides:
-            print(f'ERROR - unknown nucleotide "{nuc}" in position {position}')
+    if s[0][0] != ">":
+        print("ERROR - incorrect file format")  # Проверка файла на корректную первую строку
     else:
 
-        # Вызов функции подсчёта нуклеотидов и длины последовательности
-        Value_Nucleotides, length_DNA = nucleotide_counting(Seq_DNA)
+    # Проверка на наличие последовательности ДНК
+        if not s[1]:
+            print("ERROR - empty value")
+        else:
+
+        # Операция по созданию отдельной строки с последовательностью нуклеотидов
+            New_seq_DNA = ''.join(s[1:]).upper().replace('\n', '')
+
+
+
+            invalid_nucleotides = []
+
+        # Поиск некорректных символов и их позиции
+            for position, nuc in enumerate(New_seq_DNA, start=1):
+                if nuc not in 'GCAT':
+                    invalid_nucleotides.append((nuc, position))
+
+        # Вывод найденых некорректных символов и их позиций
+            if invalid_nucleotides:
+                for nuc, position in invalid_nucleotides:
+                    print(f'ERROR - unknown nucleotide "{nuc}" in position {position}')
+            else:
+
+            # Вызов функции подсчёта нуклеотидов и длины последовательности
+                Value_Nucleotides, length_DNA = nucleotide_counting(New_seq_DNA)
         
-        # Вывод результатов
-        print(f"Guanine - {Value_Nucleotides['G']}")
-        print(f"Cytosine - {Value_Nucleotides['C']}")
-        print(f"Adenine - {Value_Nucleotides['A']}")
-        print(f"Thymine - {Value_Nucleotides['T']}")
-        print(f"Total amount of nucleotides - {length_DNA}")
-        print(f"GC percent = {round((Value_Nucleotides['G'] + Value_Nucleotides['C']) / length_DNA * 100, 2)}%")
+            # Вывод результатов
+                print(f"Guanine - {Value_Nucleotides['G']}")
+                print(f"Cytosine - {Value_Nucleotides['C']}")
+                print(f"Adenine - {Value_Nucleotides['A']}")
+                print(f"Thymine - {Value_Nucleotides['T']}")
+                print(f"Total amount of nucleotides - {length_DNA}")
+                print(f"GC% - {round((Value_Nucleotides['G'] + Value_Nucleotides['C'])/length_DNA*100, 2)}%")
